e1b62b750a0ffa7a04111d803b291fc731fbf12a
ahmedos1999 <ah_os5@yahoo.com>
2023-11-12 19:40:56 +0200

    fixes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines the FileStorage class."""
+import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -13,8 +14,8 @@ class FileStorage:
     """Represent an abstracted storage engine.
 
     Attributes:
-    __file_path (str): The name of the file to save objects to.
-    __objects (dict): A dictionary of instantiated objects.
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
     """
     __file_path = "file.json"
     __objects = {}
@@ -24,24 +25,23 @@ class FileStorage:
         return FileStorage.__objects
 
     def new(self, obj):
-        """Set in __objects value (obj) with key (<obj_class_name>.id)."""
-        obj_name = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(obj_name, obj.id)] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
-		"""Serialize __objects to the JSON file __file_path."""
-        o_dict = FileStorage.__objects
-        obj_dict = {obj: o_dict[obj].to_dict() for obj in o_dict.keys()}
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
         with open(FileStorage.__file_path, "w") as f:
-            json.dump(obj_dict, f)
+            json.dump(objdict, f)
 
-	
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
             with open(FileStorage.__file_path) as f:
-                obj_dict = json.load(f)
-                for o in obj_dict.values():
+                objdict = json.load(f)
+                for o in objdict.values():
                     cls_name = o["__class__"]
                     del o["__class__"]
                     self.new(eval(cls_name)(**o))
